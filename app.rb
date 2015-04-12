afa527190a5680f4271a8d53048f0f7d7bf503af
Matt Cameron <mcameron@avalanche.com.au>
2015-04-12 21:58:18 +1000

    Added login page

--- app.rb
+++ app.rb
@@ -6,3 +6,7 @@ require_relative 'user'
 get '/' do
 	erb :index
 end
+
+get '/login' do
+	erb :login
+end
--- user.rb
+++ user.rb
@@ -1,3 +1,11 @@
 class User
+	attr_accessor :username, :email, :password
+
+	def initialize(username, email, password)
+		@username = username
+		@email = email
+		@password = password
+	end
+
 
 end
