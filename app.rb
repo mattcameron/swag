7068c66b2da7a969fbab2f6cc1dcd952093aba47
Matt Cameron <mcameron@avalanche.com.au>
2015-04-13 12:07:09 +1000

    Created and added mockups

--- app.rb
+++ app.rb
@@ -6,15 +6,28 @@ require_relative 'user'
 require 'pry'
 
 
+after do
+	ActiveRecord::Base.connection.close
+end
+
+
 get '/' do
 	erb :index
 end
 
-get '/login' do
+get '/signup' do
 	erb :signup
 end
 
 post '/login' do
-	User.create( username: params[:InputUsername], email: params[:InputEmail], password: params[:InputPassword])
+	User.create( username: params[:inputUsername], email: params[:inputEmail], password: params[:inputPassword])
 	redirect to ('/')
+end
+
+get '/stores' do
+	erb :store
+end
+
+post '/stores/new' do
+	Store.create (name: params[:storeName])
 end
