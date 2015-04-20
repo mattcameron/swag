065801680fd72418fc971ae2d48f9f212da602c0
Matt Cameron <mcameron@avalanche.com.au>
2015-04-20 13:02:57 +1000

    Store likes and Avg price working

--- store.rb
+++ store.rb
@@ -1,11 +1,21 @@
 class Store < ActiveRecord::Base
 	has_many :products
 
-	def self.store_likes
+	def store_likes
 		total = 0
 		self.products.each do	|product|
 			total += product.likes
 		end
 		total
+	end
+
+	def avg_price
+		total = 0
+		no_of_products = self.products.count
+		self.products.each do	|product|
+			total += product.price
+		end
+		avg = total / no_of_products
+		"%.0f" % avg
 	end
 end
